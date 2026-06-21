f897ec9842aac51e6f0782db9fbe4d1ab6078add
Tarek <t.khider@outlook.fr>
2026-06-22 00:14:33 +0200

    retry

--- terraform/cloudflare/providers.tf
+++ terraform/cloudflare/providers.tf
@@ -16,4 +16,11 @@ terraform {
 }
 
 provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+  default   = null
 }
